332d5c448314a7f638c815642a42660f6728d30c
sleroy <contact@sylvainleroy.com>
2018-05-21 11:55:36 +0200

    Exercise :  implement stadium loader

---
+++ kata-football/src/main/java/com/byoskill/trainings/cleancode/kata/football/stadium/logic/StadiumLoader.java
@@ -0,0 +1,9 @@
+package com.byoskill.trainings.cleancode.kata.football.stadium.logic;
+
+public class StadiumLoader {
+
+    public StadiumRegistry loadStadiums() {
+	// Loads stadium from the CSV stadium.csv
+	return new StadiumRegistry();
+    }
+}
---
+++ kata-football/src/main/java/com/byoskill/trainings/cleancode/kata/football/stadium/logic/StadiumRegistry.java
@@ -0,0 +1,10 @@
+package com.byoskill.trainings.cleancode.kata.football.stadium.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.byoskill.trainings.cleancode.kata.football.stadium.Stadium;
+
+public class StadiumRegistry {
+    private final List<Stadium> stadiums = new ArrayList<>();
+}
